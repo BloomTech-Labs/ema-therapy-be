4f5b0e06c254422fc348bb03884ec9dc235675fe
Brandon_Pampuch <brandonpampuch@gmail.com>
2019-11-12 18:31:08 -0600

    hid auth func in index.js until config vars added

--- index.js
+++ index.js
@@ -21,19 +21,19 @@ app.use(morgan("combined"));
 
 
 //function fro checking JWT
-const checkJwt = jwt({
-    secret: jwksRsa.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
-    }),
+// const checkJwt = jwt({
+//     secret: jwksRsa.expressJwtSecret({
+//         cache: true,
+//         rateLimit: true,
+//         jwksRequestsPerMinute: 5,
+//         jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+//     }),
 
-    // Validate the audience and the issuer.
-    audience: authConfig.audience, // name of api //identifier in settings
-    issuer: `https://${authConfig.domain}/`, //local host
-    algorithms: ['RS256']
-});
+//     // Validate the audience and the issuer.
+//     audience: authConfig.audience, // name of api //identifier in settings
+//     issuer: `https://${authConfig.domain}/`, //local host
+//     algorithms: ['RS256']
+// });
 
 
 app.use('/backend', graphqlHTTP({
