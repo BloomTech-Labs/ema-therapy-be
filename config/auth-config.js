b664ba6c1e52a58e43c0c95f786b1496ee77d918
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-11-18 20:42:27 -0600

    adds FORCE_NODE_ENV check

--- config/auth-config.js
+++ config/auth-config.js
@@ -4,15 +4,17 @@ let AUTH0_CLIENT_ID;
 let AUTH0_DOMAIN;
 let AUTH0_AUDIENCE;
 
-if (process.env.NODE_ENV === 'development') {
+const NODE_ENV = process.env.FORCE_NODE_ENV || process.env.NODE_ENV;
+
+if (NODE_ENV === 'development') {
   AUTH0_AUDIENCE = 'http://localhost:5000';
   AUTH0_CLIENT_ID = 'oVhKrQPd2E4o0n9mPPtV1rfoKJaiMYNi';
   AUTH0_DOMAIN = 'moodmuse-dev.auth0.com';
-} else if (process.env.NODE_ENV === 'production') {
+} else if (NODE_ENV === 'production') {
   AUTH0_AUDIENCE = 'https://moodmuse.herokuapp.com';
   AUTH0_CLIENT_ID = 'RWYKn1NSBG6LjReho2WItTjxxdtBxjXj';
   AUTH0_DOMAIN = 'moodmuse.auth0.com';
-} else if (process.env.STAGING_ENV === true) {
+} else if (NODE_ENV === 'staging') {
   AUTH0_AUDIENCE = 'https://moodmuse-staging.herokuapp.com';
   AUTH0_CLIENT_ID = 'KwOV32dmJl1hXtMWAkaf8AgV4NNjYdPW';
   AUTH0_DOMAIN = 'moodmuse-staging.auth0.com';
