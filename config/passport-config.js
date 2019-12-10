db5fe3b36bbba5ad00a08ce73cb49c8be64f3892
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-12-10 11:07:22 -0800

    add google strategy to passport-config.js

--- config/passport-config.js
+++ config/passport-config.js
@@ -21,4 +21,42 @@ module.exports = (passport) => {
         .catch((err) => console.log(err));
     }),
   );
+
+  passport.use(
+    new GoogleStrategy(
+      {
+        // options for the google strategy
+        callbackURL: '/auth/google/redirect',
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret,
+      },
+      (accessToken, refreshToken, profile, done) => {
+        // passport callback function
+        // check if user already exists
+        User.findOne({ googleId: profile.id }).then((existingUser) => {
+          console.log('profile:', profile);
+          if (existingUser) {
+            //if they exist, get them
+            console.log('existing user found:', existingUser);
+            done(null, existingUser);
+          } else {
+            // if not, create user in db
+            try {
+              newUser = User.create({
+                username: profile.displayName,
+                googleId: profile.id,
+                thumbnail: profile._json.picture,
+              }).then((newUser) => {
+                console.log('New user created:', newUser);
+                done(null, newUser);
+              });
+            } catch (err) {
+              console.log('Error creating user:', err);
+              done(err, null);
+            }
+          }
+        });
+      },
+    ),
+  );
 };
