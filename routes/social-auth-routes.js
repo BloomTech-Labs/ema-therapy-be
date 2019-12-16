4a0d0cb1e71287d4e8bf0159e989cf95be160331
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-12-16 09:25:24 -0800

    add html and script to social auth routes

--- routes/social-auth-routes.js
+++ routes/social-auth-routes.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const makeToken = (user, cb) => {
+  let payload = {
+    email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
+  };
+
+  console.log('payload is: ', payload);
+
+  // Sign token
+  jwt.sign(
+    payload,
+    process.env.SECRET_OR_KEY,
+    {
+      expiresIn: 60 * 60 * 24 * 7, // 1 week in seconds
+    },
+    (err, token) => {
+      if (err) throw err;
+      cb(token);
+    },
+  );
+};
 
 // auth with google
 router.get(
@@ -19,8 +43,22 @@ router.get(
     session: false,
   }),
   (req, res) => {
-    console.log(req);
-    res.redirect('http://localhost:3000/dashboard');
+    makeToken(req.user, (token) => {
+      console.log('jwt log:', jwt);
+      const htmlWithToken = `
+    <html>
+      <script>
+        // save JWT to local storage
+        window.localStorage.setItem('token', '${token}')
+        // redirect browser to dashboard of application
+        console.log('stuff is doing stuff in html script')
+        window.location.href = 'http://localhost:3000/dashboard';
+      </script>
+    </html>
+    `;
+
+      res.send(htmlWithToken);
+    });
   },
 );
 
