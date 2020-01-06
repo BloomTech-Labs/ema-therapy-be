8606e61d7ed4aac981ba4d1b84415e6cd61b71c4
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-01-06 13:26:35 -0600

    updates to authentication error handling

--- validation/login-validation.js
+++ validation/login-validation.js
@@ -10,14 +10,16 @@ module.exports = function validateLoginInput(data) {
 
   // Email checks
   if (Validator.isEmpty(data.email)) {
-    errors.email = 'Email field is required';
+    // errors.email = 'Email field is required';
+    errors.error = 'Email field is required';
   } else if (!Validator.isEmail(data.email)) {
-    errors.email = 'Email is invalid';
+    // errors.email = 'Email is invalid';
+    errors.error = 'Email is invalid';
   }
 
   // Password checks
   if (Validator.isEmpty(data.password)) {
-    errors.password = 'Password field is required';
+    errors.error = 'Password field is required';
   }
 
   return {
--- validation/register-validation.js
+++ validation/register-validation.js
@@ -5,32 +5,32 @@ module.exports = function validateRegisterInput(data) {
   let errors = {};
 
   // Convert empty fields to an empty string so we can use validator functions
-  data.name = !isEmpty(data.name) ? data.name : '';
+  data.firstName = !isEmpty(data.firstName) ? data.firstName : '';
   data.email = !isEmpty(data.email) ? data.email : '';
   data.password = !isEmpty(data.password) ? data.password : '';
   data.password2 = !isEmpty(data.password2) ? data.password2 : '';
 
   // Name checks
-  // if (Validator.isEmpty(data.name)) {
-  //   errors.name = 'Name field is required';
-  // }
+  if (Validator.isEmpty(data.firstName)) {
+    errors.error = 'Name field is required';
+  }
 
   // Email checks
   if (Validator.isEmpty(data.email)) {
-    errors.email = 'Email field is required';
+    errors.error = 'Email field is required';
   } else if (!Validator.isEmail(data.email)) {
-    errors.email = 'Email is invalid';
+    errors.error = 'Email is invalid';
   }
 
   // Password checks
   if (Validator.isEmpty(data.password)) {
-    errors.password = 'Password field is required';
+    errors.error = 'Password field is required';
   }
   // if (Validator.isEmpty(data.password2)) {
   //   errors.password2 = 'Confirm password field is required';
   // }
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = 'Password must be at least 6 characters';
+    errors.error = 'Password must be at least 6 characters';
   }
   // if (!Validator.equals(data.password, data.password2)) {
   //   errors.password2 = 'Passwords must match';
